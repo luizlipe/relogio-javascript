8515b4347e2f5b9af5503931fd2314d6b92def04
luizlipe <felipe250718@gmail.com>
2024-11-27 11:02:26 -0300

    :feat:funcionamento do relogio analógico

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 let digitalElement = document.querySelector('.digital')
 let sElement = document.querySelector('.p_s')
-let mElement = document.querySelector('.p_s')
-let hElement = document.querySelector('.p_s')
+let mElement = document.querySelector('.p_m')
+let hElement = document.querySelector('.p_h')
 
 function updateClock(){
     let now = new Date()
@@ -11,6 +11,15 @@ function updateClock(){
 
 
     digitalElement.innerHTML = `${fixZero(hour)}:${fixZero(minute)}:${fixZero(second)}`
+
+    let sDeg = ((360/60) * second) - 90
+    let mDeg = ((360/60) * minute) - 90
+    let hDeg = ((360 / 12) * hour) -90
+
+    sElement.style.transform = `rotate(${sDeg}deg)`
+    mElement.style.transform = `rotate(${mDeg}deg)`
+    hElement.style.transform = `rotate(${hDeg}deg)`
+
 }
 
 function fixZero(time){
@@ -18,3 +27,4 @@ function fixZero(time){
 }
 
 setInterval(updateClock, 1000)
+updateClock()
